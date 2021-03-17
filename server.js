a4d76ca73fb7f8efec98590ecc1fab2aa03c85ef
ben-jamming-reilly <benreilly0809@gmail.com>
2021-03-17 12:56:38 -0700

    Cleaned up code and added comments

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const config = require("config");
 const app = express();
 app.use(bodyParser.json());
 
-// Authentication and user routes
+// Authentication & User Routes
 
 app.post("/api/register", async (req, res) => {
   const form = req.body;
@@ -17,20 +17,25 @@ app.post("/api/register", async (req, res) => {
   try {
     const [rows, fields] = await db.query("SELECT user_id FROM User WHERE email = ?; ", [form.email]);
 
+    // Checks if email has already been registered
     if (rows.length !== 0) {
       return res.status(403).json({ errors: [{ msg: "Cannot register the same email twice. " }] });
     }
 
-    // password 1234gozags -> *hash*
-    // *hash* -> 1234gozags
+    // Creates a secure hashed and salted password
     const salt = await bcrypt.genSalt(10);
     const hashPass = await bcrypt.hash(form.password, salt);
+
+    // Creates an user id
     const id = uuidv4();
 
+    // Creates a sercure hashed and salted password
     await db.query("INSERT INTO User (user_id, name, email, password, join_date, last_login_date) " +
       "VALUES (?, ?, ?, ?, NOW(), NOW())",
       [id, form.name, form.email, hashPass]);
 
+
+    // Sign and sends an auth token
     const payload = {
       user: {
         id: id,
@@ -58,11 +63,13 @@ app.post("/api/login", async (req, res) => {
   try {
     const [rows, fields] = await db.query("SELECT user_id, password FROM User WHERE email = ?; ", [form.email]);
 
+    // Check if an email is registered
     if (rows.length !== 1)
       return res.status(400).json({ errors: [{ msg: "Invalid credentials" }] });
 
     const isMatch = await bcrypt.compare(form.password, rows[0].password);
 
+    // Confirms that the user entered in the correct password 
     if (!isMatch) {
       return res.status(400).json({ errors: [{ msg: "Invalid credentials" }] });
     }
@@ -71,6 +78,7 @@ app.post("/api/login", async (req, res) => {
     await db.query("UPDATE User SET last_login_date = NOW() WHERE user_id = ?; ",
       [rows[0].user_id]);
 
+    // Sign and sends an auth token
     const payload = {
       user: {
         id: rows[0].user_id,
@@ -91,13 +99,6 @@ app.post("/api/login", async (req, res) => {
     return res.status(500).send("Server Error");
   }
 });
-
-/*
-app.get("/", (req, res) => {
-  return res.status(200).json({ msg: "Hello" })
-});
-*/
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log("Server started on port " + PORT));
