defaf1f4568f96d5f19fe3f92ea22a3f1f26b535
ben-jamming-reilly <benreilly0809@gmail.com>
2021-03-17 18:59:57 -0700

    Finished up backend routes

--- server.js
+++ server.js
@@ -5,11 +5,28 @@ const { v4: uuidv4 } = require("uuid");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
+const auth = require("./middleware/auth");
 
 const app = express();
 app.use(bodyParser.json());
 
 // Authentication & User Routes
+
+app.get("/api/", auth, async (req, res) => {
+  const id = req.user.id;
+
+  try {
+    const [
+      rows,
+      fields
+    ] = await db.query("SELECT * FROM User WHERE user_id = ?", [id]);
+
+    return res.json(rows[0]);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Server Error");
+  }
+});
 
 app.post("/api/register", async (req, res) => {
   const form = req.body;
@@ -94,6 +111,68 @@ app.post("/api/login", async (req, res) => {
         else return res.status(200).json({ token });
       }
     );
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Server Error");
+  }
+});
+
+// Item routes
+
+app.get("/api/item", auth, async (req, res) => {
+  const id = req.user.id;
+
+  try {
+    const [
+      rows,
+      fields
+    ] = await db.query("SELECT * FROM Item WHERE user_id = ?; ",
+      [id]
+    );
+    return res.status(200).json(rows);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Server Error");
+  }
+});
+
+app.post("/api/item", auth, async (req, res) => {
+  // Adds an item to the todo list
+  const id = req.user.id;
+  const form = req.body;
+
+  try {
+    await db.query("INSERT INTO Item (user_id, post_date, body)" +
+      " VALUES (?, NOW(), ?)",
+      [id, form.body]
+    );
+
+    return res.status(201).json({ msg: "Item added to list" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Server Error");
+  }
+});
+
+app.delete("/api/item/:id", auth, async (req, res) => {
+  const user_id = req.user.id;
+  const item_id = req.params.id;
+
+  try {
+    const [
+      rows,
+      fields
+    ] = await db.query("SELECT user_id FROM Item WHERE user_id = ? AND item_id = ?",
+      [user_id, item_id]
+    );
+
+    if (rows.length != 1)
+      return res.status(401).json({ errors: [{ msg: "Sorry can't delete that item :(" }] });
+
+    await db.query("DELETE FROM Item WHERE item_id = ?", [item_id]);
+
+    return res.status(200).json({ msg: "Item was deleted " });
+
   } catch (err) {
     console.error(err);
     return res.status(500).send("Server Error");
